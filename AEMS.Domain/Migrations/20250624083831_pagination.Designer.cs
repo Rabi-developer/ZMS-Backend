4337668d6837274dd8cdc7d09f989b1031e10589
Rabail Waheed <rabailw99@gmail.com>
2025-06-24 16:06:05 +0500

    pagination

--- AEMS.Domain/Migrations/20250624083831_pagination.Designer.cs
+++ AEMS.Domain/Migrations/20250624083831_pagination.Designer.cs
@@ -4,6 +4,7 @@ using IMS.Domain.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IMS.Domain.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250624083831_pagination")]
+    partial class pagination
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -264,7 +267,7 @@ namespace IMS.Domain.Migrations
                         {
                             Id = new Guid("fc9544a9-4e5c-4032-a27f-3001b29364c5"),
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "bb609df0-a740-4704-ada0-f967df5fb104",
+                            ConcurrencyStamp = "5c912672-af9d-4837-b2b5-a38fec76b9f3",
                             CreatedBy = new Guid("00000000-0000-0000-0000-000000000000"),
                             CreatedDateTime = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             Email = "admin@AEMS.com",
@@ -276,7 +279,7 @@ namespace IMS.Domain.Migrations
                             LockoutEnabled = false,
                             NormalizedEmail = "ADMIN@AEMS.COM",
                             NormalizedUserName = "SUPERADMIN",
-                            PasswordHash = "AQAAAAIAAYagAAAAEPSAzRLJrAeopCgKnhaQNLf65hGJBRwiJTF8BwosrCZ2Yu2Pxv2lbAW6OHYCqIhS8A==",
+                            PasswordHash = "AQAAAAIAAYagAAAAEHQ+ci+APinzMRthegrwWsipQsVc4lCkR8lIFYSwjTwn40HuPURyLZQPDokFoWOEHA==",
                             PhoneNumberConfirmed = false,
                             SecurityStamp = "d3290d28-d69c-4f25-bbed-d30a1f7a9d5c",
                             TwoFactorEnabled = false,

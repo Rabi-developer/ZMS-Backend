156348b0d319184270c32b35ed726b2778c62ed2
Rabail Waheed <rabailw99@gmail.com>
2025-09-06 18:21:35 +0500

    ABL - Consignment : CreditAllowed New Field Add

--- AEMS.Business/DTOs/Responses/ConsignmentRes.cs
+++ AEMS.Business/DTOs/Responses/ConsignmentRes.cs
@@ -16,6 +16,7 @@ namespace ZMS.Domain.Entities
         public string? ConsignmentNo { get; set; }
         public string? Consignor { get; set; }
         public string? ConsignmentDate { get; set; }
+        public string? CreditAllowed { get; set; }
         public string? Consignee { get; set; }
         public string? ReceiverName { get; set; }
         public string? ReceiverContactNo { get; set; }

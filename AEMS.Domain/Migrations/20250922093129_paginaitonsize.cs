c60c761e12c66224df3b16f8d1c1b7f7e8c440a9
Rabail Waheed <rabailw99@gmail.com>
2025-09-22 14:44:26 +0500

    ABL - Pagination Size 10 to 100

---
+++ AEMS.Domain/Migrations/20250922093129_paginaitonsize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZMS.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class paginaitonsize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("fc9544a9-4e5c-4032-a27f-3001b29364c5"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "f8e0243c-c8e3-4430-a6f6-5f286e3d7da7", "AQAAAAIAAYagAAAAEMi8n1T3otORccsnQkz2uwpo2xuoaG2xhXMTuFT//V+IzqOpIZFJHeLz1A0LWj0R1A==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("fc9544a9-4e5c-4032-a27f-3001b29364c5"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "6e0a48a2-e928-41f8-902c-eb7961e599a9", "AQAAAAIAAYagAAAAEBcGzbYchpwokW7OQz3Eif4mBnLju1RpkOq2DEB1Au/evLPo9OH3WH2NfAdUopqppg==" });
+        }
+    }
+}
--- AEMS.Domain/Utilities/Pagination.cs
+++ AEMS.Domain/Utilities/Pagination.cs
@@ -5,7 +5,7 @@ public class Pagination
     public int? TotalPages { get; set; }
     public int? Total { get; set; }
     public int? PageIndex { get; set; } = 1;
-    public int? PageSize { get; set; } = 10;
+    public int? PageSize { get; set; } = 100;
     public string? RefId { get; set; }
     public string? SearchQuery { get; set; }
     public string? TotalCount { get; set; }

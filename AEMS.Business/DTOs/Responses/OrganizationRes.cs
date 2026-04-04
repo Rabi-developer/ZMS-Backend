6a118cf5823267cb8b3e0f52b45527a719d4f5ff
Rabail Waheed <r.offical99@gmail.com>
2026-04-04 10:22:31 +0500

    ZMS - Comapny Detail

--- AEMS.Business/DTOs/Responses/OrganizationRes.cs
+++ AEMS.Business/DTOs/Responses/OrganizationRes.cs
@@ -5,20 +5,15 @@ namespace IMS.Business.DTOs.Responses;
 public class OrganizationRes
 {
     public Guid Id { get; set; }
-    [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public string? Description { get; set; }
-
-    [EmailAddress]
-    public string Email { get; set; }
-
-    [Url]
-    public string Website { get; set; }
-    public string AddressLine1 { get; set; }
+    public string? Email { get; set; }
+    public string? Website { get; set; }
+    public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string Country { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
     public string? Zip { get; set; }
 }
 
---
+++ AEMS.Domain/Migrations/20260404052206_hfab.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZMS.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class hfab : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Website",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine1",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("fc9544a9-4e5c-4032-a27f-3001b29364c5"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "576201f9-b426-409d-9f7f-b26d8574ce75", "AQAAAAIAAYagAAAAEH4hpBnfu6wqx9g3ps1mjsmg0+LKlHbV6GxbZkAbJBAIDzSHS286Kl30MFPkl4Tclg==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Website",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine1",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("fc9544a9-4e5c-4032-a27f-3001b29364c5"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "1e5a9719-100c-4b1a-a61b-b396c0e2eed9", "AQAAAAIAAYagAAAAEJINCeC6DI78RvT5X9TuqSosRN3JxJqVDQFpi8KYzuupJSsdTWzuX+5I+E7GgkAghA==" });
+        }
+    }
+}
